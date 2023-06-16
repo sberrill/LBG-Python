db0563f82d0915f71bb050d0e8823368d7fd771d
sberrill <stevebezza@hotmail.com>
2023-06-16 11:54:41 +0100

    updated lby.py

--- lbg.py
+++ lbg.py
@@ -5,6 +5,8 @@ NB. mixes LBYL and EAFP to reflect simpler logic
 This version using simple prints to STDOUT rather than formalised logger
 """
 
+# this is a change to the file
+
 # import argparse for passing PORT variable through the cli
 import argparse
 
